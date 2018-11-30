1aee02d65ca168282392800dd684eb7514256613
ThilinaManamgoda <maanafunedu@gmail.com>
2018-11-30 15:31:01 +0530

    Add test repo

---
+++ tests/sample-scenario-tests/src/test/java/org/wso2/SimpleScenarioTest.java
@@ -0,0 +1,55 @@
+/*
+ *  Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
+package org.wso2;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.HttpStatus;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+/**
+ * This class tests the HelloWorld Rest endpoint
+ */
+
+public class SimpleScenarioTest {
+    @Test
+    public void testEndpoint() {
+        HttpClient client = new HttpClient();
+        HttpMethod method = new GetMethod(System.getProperty("endpoint"));
+        try {
+            int statusCode = client.executeMethod(method);
+            Assert.assertEquals(statusCode, HttpStatus.SC_OK);
+        } catch (HttpException e) {
+            System.err.println("Fatal protocol violation: " + e.getMessage());
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.err.println("Fatal transport error: " + e.getMessage());
+            e.printStackTrace();
+        } finally {
+            // Release the connection.
+            method.releaseConnection();
+        }
+    }
+
+}

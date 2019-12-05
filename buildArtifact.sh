24d57b0e9d4e51ecdbacba44ab05d09908d2f505
ShafanaSafwan <kreepyprincess07@outlook.com>
2019-12-05 12:56:04 +0530

    direct maven logs to file

--- buildArtifact.sh
+++ buildArtifact.sh
@@ -30,25 +30,25 @@ fi
 cd $ARTIFACT_LOCATION/helloworld/dev/dev-esb
 mvn clean install
 cd ../helloworld_dev
-mvn clean install
+mvn clean install > artifact.log
 cp -r target/*  $ARTIFACT_LOCATION/helloworld/artifacts/
 
 cd $ARTIFACT_LOCATION/helloworld/staging/staging-esb
 mvn clean install
 cd ../helloworld_staging
-mvn clean install
+mvn clean install > artifact.log
 cp -r target/*  $ARTIFACT_LOCATION/helloworld/artifacts/
 
 cd $ARTIFACT_LOCATION/helloworld/prod/prod_esb
 mvn clean install
 cd ../helloworld_production
-mvn clean install
+mvn clean install > artifact.log
 cp -r target/*  $ARTIFACT_LOCATION/helloworld/artifacts/
 
 cd $ARTIFACT_LOCATION/helloworld/common/helloworld
 mvn clean install
 cd ../helloworld_common
-mvn clean install
+mvn clean install > artifact.log
 cp -r target/*  $ARTIFACT_LOCATION/helloworld/artifacts/
 
 cp -r $ARTIFACT_LOCATION/helloworld/artifacts/* $IMAGE_RESOURCES
